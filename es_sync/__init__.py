69cba21ea341d7e979c35e1394ec362068740c39
igorcoding <igorcoding@gmail.com>
2016-07-16 18:51:58 +0300

    config path to __init__

--- es_sync/__init__.py
+++ es_sync/__init__.py
@@ -49,9 +49,12 @@ class ElasticSync(object):
         rv = bool(self.log_file and self.log_pos)
         return rv
 
-    def __init__(self):
+    def __init__(self, config_path=None):
+        self.config_path = config_path
+        if self.config_path is None:
+            self.config_path = sys.argv[1]
         try:
-            self.config = yaml.load(open(sys.argv[1]))
+            self.config = yaml.load(open(self.config_path))
         except IndexError:
             print('Error: not specify config file')
             exit(1)
